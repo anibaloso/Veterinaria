8c4c17222845715f8d32e7bd3d5a8de942652219
anibal <anibaloso@live.com>
2025-08-01 23:52:59 -0400

    Agrega funcionalidad a nueva cita, se pobla la tabla con las citas guardadas

--- app.js
+++ app.js
@@ -26,3 +26,51 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+// recolectando datos del formulario
+const pacientes = [];
+function recolectarDatos() {
+    const nombreMascota = document.getElementById('nomMascota').value;
+    const nombreDueño = document.getElementById('nomOwner').value;
+    const contacto = document.getElementById('pacienteContacto').value;
+    const especie = document.getElementById('pacienteEspecie').value;
+    const mensaje = document.getElementById('mensaje').value;
+    const fecha = document.getElementById('fecha').value;
+    const horario = document.getElementById('horario').value;
+    const paciente = {
+        nombreMascota,
+        nombreDueño,
+        contacto,
+        especie,
+        mensaje,
+        fecha,
+        horario
+    };
+    pacientes.push(paciente);
+    llenarTabla();
+    document.getElementById('mi-modal').classList.remove('is-active');
+    console.log(pacientes);
+}
+
+// llenado de tabla
+function llenarTabla() {
+    document.getElementById('fila').innerHTML = ''; // Limpiar la tabla antes de llenarla
+    pacientes.forEach(paciente => {
+        //en el id paciente llenare con el pacientes.nomPaciente
+        document.getElementById('fila').innerHTML += ` 
+            <tr>
+                <td>${paciente.nombreMascota} </td>
+                <td> ${paciente.especie}</td>
+                <td> ${paciente.nombreDueño}</td>
+                <td> ${paciente.contacto}</td>
+                <td> ${paciente.mensaje}</td>
+                <td> ${paciente.fecha} a las ${paciente.horario}</td>
+                <td>
+                    <button class="button is-warning" onclick="modificarHora()">...</button>
+                    <button class="button is-danger is-dark" onclick="eliminarHora()">X</button>
+                </td>
+            </tr>
+        `
+    })
+}
